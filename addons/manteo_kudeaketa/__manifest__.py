610767130b5aba46f3506574c936575dcc4cbec9
UnaiCarnes25paag <ucarnesat25paag@ikzubirimanteo.com>
2026-02-09 13:12:08 +0100

    Modulo creado

---
+++ addons/manteo_kudeaketa/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Xabier-Zubiri-Manteo Kudeaketa',
+    'version': '1.0',
+    'summary': 'Ikastetxeko kudeaketa sistema',
+    'description': """
+Xabier-Zubiri-Manteo institutuarentzako
+ikasle, irakasle eta ekipamenduen kudeaketa.
+    """,
+    'author': 'Zure taldea',
+    'category': 'Education',
+    'depends': ['base'],
+    'data': [],
+    'installable': True,
+    'application': True,
+}
